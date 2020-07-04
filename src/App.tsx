431e2a4244033b3b16b400a5009e21f374a33ae0
quinsberry <fatbobisreal@gmail.com>
2020-07-04 18:27:34 +0200

    added formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
@@ -17,3 +18,31 @@ const App = () => {
 }
 
 export default App
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> Initialize project using Create React App
